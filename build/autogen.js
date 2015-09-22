7111ebd282756230c8f8a6fa935465b8acd7570a
Hristo Deshev <hristo@deshev.com>
2015-09-22 19:52:49 +0300

    Autogenerate dts dependency walks in ninja build

--- build/autogen.js
+++ build/autogen.js
@@ -4,7 +4,20 @@ var globule = require('globule');
 var ninja = buildGen('1.6');
 
 ninja.assign('tsflags', '--module commonjs --noEmitOnError --noImplicitAny --emitDecoratorMetadata --experimentalDecorators --target es5');
-ninja.rule('tsc').run('./node_modules/.bin/tsc $tsflags $in').description('TypeScript compile: $in');
+
+ninja.rule('tsc').run('./node_modules/.bin/tsc $tsflags "$in" && node build/getrefs.js "$in" "$out" "$in.dep"').depfile('"$in.dep"').description('TypeScript compile: $in');
+
+ninja.rule('dts').run('touch "$out"').description('TypeScript declarations: $in');
+
+var dtsFiles = globule.findMapping(
+    ['**/*.d.ts'],
+    {srcBase: 'src', destBase: '.ninja_dts', ext: '.ts.build', extDot: 'last'}
+);
+
+for (var i = 0; i < dtsFiles.length; i++) {
+    var match = dtsFiles[i];
+    ninja.edge(match.dest).from(match.src).using('dts');
+}
 
 var typescriptFiles = globule.findMapping(
     ['**/*.ts', '!**/*.d.ts'],
