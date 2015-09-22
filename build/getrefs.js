486d83977698fdf444bf08fa1055b64c0f418793
Hristo Deshev <hristo@deshev.com>
2015-09-22 19:34:31 +0300

    Extract references from ts files.
    Generate gcc-like makefiles that get parsed by ninja.

---
+++ build/getrefs.js
@@ -0,0 +1,39 @@
+if (process.argv.length != 5) {
+    console.log('USAGE: node getrefs.js <file> <depfile>');
+    process.exit(1);
+}
+
+var fs = require('fs');
+var path = require('path');
+
+var refMatcher = /\/\/\/\s*<reference.*path\s*=\s*["']([^"\']+).*/g;
+var infile = process.argv[2];
+var outfile = process.argv[3];
+var depfile = process.argv[4];
+
+fs.readFile(infile, 'utf8', function(err, data) {
+    if (err) {
+        console.log(err.message);
+        process.exit(1);
+    }
+
+    var foundDeps = [];
+    data.replace(refMatcher, function(_, path) {
+        foundDeps.push(path);
+    });
+
+    if (foundDeps.length > 0) {
+        var depTimeStamps = foundDeps.map(function(foundDep) {
+            return path.join('.ninja_dts', foundDep + '.build');
+        });
+        var depContents = outfile + ': ' + infile + ' ' + depTimeStamps.join(' ')
+
+        fs.writeFile(depfile, depContents, function(err) {
+            if(err) {
+                console.log(err.message);
+                process.exit(1);
+            }
+            process.exit(0);
+        });
+    }
+});
